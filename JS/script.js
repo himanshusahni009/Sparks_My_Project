4a2e1fee92f1f1e88b5eee0de63ea8554ac6e28b
Himanshu Sahni <70528665+himanshusahni009@users.noreply.github.com>
2021-07-15 17:47:27 +0530

    Add files via upload

---
+++ JS/script.js
@@ -0,0 +1,15 @@
+/* This code is Created by: Himanshu Sahni */
+
+// Function to Open Navigation Bar
+function openMe() {
+	x = document.getElementById("click");
+	x.className = "nav_bar opened";
+}
+
+// Function to Close Navigation Bar
+function closeMe() {
+	x = document.getElementById("click");
+	x.className = "nav_bar closed";
+}
+
+/* This code is Created by: Himanshu Sahni */
